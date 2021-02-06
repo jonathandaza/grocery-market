9351ba1be669c2e3bcb8d9bd708b76c99a9eb4f4
Jonathan <jdazav.5@gmail.com>
2021-02-06 21:48:17 +1300

    Initial commit

---
+++ grocery-market/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grocery_market.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Rules = new List<Rule>();
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<Rule> Rules { get; set; }
+    }
+}
